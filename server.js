50296abc219612707d17c269f33b08839b52432e
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-25 23:14:45 +0200

    Install bluebird promise lib

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ let express = require('express'),
 app = express(),
 config = require('./config/config.js'),
 mongoose = require('mongoose'),
+Promise = require('bluebird'),
 db,
 server;
+
+mongoose.Promise = Promise;
 
 server = app.listen(config.port);
 console.log('Server is listening on', config.port);
