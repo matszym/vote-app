c46b55ecfe47aaa2cfca43bb8ddc60f91dc8781c
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-26 19:46:13 +0200

    Add twitter api keys to config file

--- config/development/config.js
+++ config/development/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   port: 9000,
-  mongo: 'mongodb://localhost/vote_app'
+  mongo: 'mongodb://localhost/vote_app',
+  twitter: {
+    consumerKey: process.env.VOTE_APP_TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.VOTE_APP_TWITTER_CONSUMER_SECRET
+  }
 }
