399334e6b3c8b49db8d32653ef6a2157c3af406b
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-01 13:49:29 +0200

    User server controller

---
+++ controllers/user.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let Promise = require('bluebird'),
+passport = require('passport');
+
+exports.signin = (req, res, next) => {
+  let authenticate = Promise.promisify(passport.authenticate('twitter'));
+  console.log('inside controller');
+  authenticate(req, res, next)
+  .then((user, info) => {
+    if(!user) {
+      return res
+        .status(401)
+        .send({
+          message: 'Authentication failed'
+        });
+    } else {
+      let logIn = Promise.promisify(req.logIn);
+
+      logIn(user)
+      .then(() => res.send({
+        message: 'You are now logged in',
+        user
+      }))
+      .catch(next);
+    }
+  })
+  .catch(next);
+}
+
+exports.isAuthenticated = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res
+    .status(401)
+    .send([{
+      content: 'Please log in first',
+      type: 'danger'
+    }])
+  } else {
+    next();
+  }
+}
