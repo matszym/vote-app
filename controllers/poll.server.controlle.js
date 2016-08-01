c0ba4a1184d18dc8c4fcb3bba873850862aa53ee
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-01 13:50:37 +0200

    poll server controller

---
+++ controllers/poll.server.controlle.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.createPool = (req, res) => {
+
+}
+
+exports.getPool = (req, res) => {
+
+}
+
+exports.getAllResults = (req, res) => {
+
+}
+
+exports.deletePool = (req, res) => {
+
+}
