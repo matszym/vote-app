d0695e02f43b440a2fefcf76f83ab94a7e67eb24
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-01 13:48:21 +0200

    Add routes for passport

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 'use strict';
 
+
 let express = require('express'),
 app = express(),
+session = require('express-session'),
+MongoStore = require('connect-mongo')(session),
 config = require('./config/config.js'),
+passport = require('passport'),
 mongoose = require('mongoose'),
 Promise = require('bluebird'),
 db,
 server;
 
-mongoose.Promise = Promise;
+require('./config/passport.js')();
+let userCtrl = require('./controllers/user.server.controller.js');
+
+Promise.promisifyAll(mongoose);
 db = mongoose.connect(config.mongo);
+
+app.use(session({
+  secret: config.sessionSecret,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
+}));
+app.use(passport.initialize());
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 server = app.listen(config.port);
 console.log('Server is listening on', config.port);
