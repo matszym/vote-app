afaee034c85ddc320c2513a76312577ed17a8580
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-01 20:33:30 +0200

    api poll routes

---
+++ routes/poll.server.routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const pollCtrl = require('../controllers/poll.server.controller.js'),
+userCtrl = require('../controllers/user.server.controller.js');
+
+module.exports = app => {
+  
+  app.route('/api/poll')
+  .post(userCtrl.isAuthenticated, pollCtrl.createPoll);
+
+  app.route('/api/polls')
+  .get(pollCtrl.getPolls);
+
+  app.route('/api/poll/:id')
+  .get(pollCtrl.getPoll)
+  .delete(userCtrl.isAuthenticated, pollCtrl.deletePoll);
+}
