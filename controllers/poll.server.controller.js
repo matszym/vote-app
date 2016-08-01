453532b2fec4288445e614e15051ac861bb56d94
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-01 20:31:55 +0200

    file rename (typo fix)

---
+++ controllers/poll.server.controller.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Poll = require('../models/poll.server.model.js'),
+msg = require('./message.server.controller.js');
+
+exports.createPoll = (req, res) => {
+  Poll.createAsync(req.body.poll)
+  .then(poll => {
+    res
+    .send(msg('Your poll was created.'))
+  })
+  .catch(err => {
+    res
+    .status(400)
+    .send(err);
+  });
+}
+
+exports.getPoll = (req, res) => {
+  Poll.findOneByIdAsync(req.params.id)
+  .then(poll => res.send(poll))
+  .catch(err => {
+    res
+    .status(400)
+    .send(err);
+  })
+}
+
+exports.getPolls = (req, res) => {
+  Poll.find({})
+  .sort('-created')
+  .limit(req.params.limit)
+  .skip(req.params.offset)
+  .execAsync()
+  .then(polls => res.send(polls))
+  .catch(err => {
+    res
+    .status(400)
+    .send(msg('We experienced technical dificulties feetching pools. Please try again later', 'danger'))
+  })
+}
+
+exports.getAllResults = (req, res) => {
+  Poll.findAsync({_creator: req.user})
+  .sort('-created')
+  .limit(req.params.limit)
+  .offset(req.params.offset)
+  .then(results => res.send(polls))
+  .catch(err => {
+    res
+    .status(400)
+    .send(msg('We could not feetch your polls results. Please try again later', 'danger'));
+  })
+}
+
+exports.deletePoll = (req, res) => {
+  Poll.removeAsync({_id: req.params.id})
+  .then(poll => res.send(msg(`Poll ${poll.name} removed.`)))
+  .catch(err => {
+    res
+    .status(400)
+    .send('We could not remove poll. Please try again later.');
+  });
+}
