1434fe9b2e2480bbe198dfa28946e3f91bc0683e
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-01 13:47:02 +0200

    passport config variables

--- config/development/config.js
+++ config/development/config.js
@@ -3,6 +3,8 @@ module.exports = {
   mongo: 'mongodb://localhost/vote_app',
   twitter: {
     consumerKey: process.env.VOTE_APP_TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.VOTE_APP_TWITTER_CONSUMER_SECRET
-  }
+    consumerSecret: process.env.VOTE_APP_TWITTER_SECRET_KEY,
+    callbackURL: 'http://127.0.0.1:9000/auth/twitter/callback'
+  },
+  sessionSecret: 'Unsecure localhost secret'
 }
