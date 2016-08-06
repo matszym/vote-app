5e385432ce790cb3fc328f3827453453ef696a4d
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-07 00:07:40 +0200

    add loggind and routes to server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 let express = require('express'),
 app = express(),
+morgan = require('morgan'),
 session = require('express-session'),
 MongoStore = require('connect-mongo')(session),
 config = require('./config/config.js'),
@@ -18,19 +19,24 @@ let userCtrl = require('./controllers/user.server.controller.js');
 Promise.promisifyAll(mongoose);
 db = mongoose.connect(config.mongo);
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan('dev'));
+}
+
 app.use(session({
   secret: config.sessionSecret,
   store: new MongoStore({
     mongooseConnection: mongoose.connection
   }),
-  resave: false,
+  resave: true,
   saveUninitialized: true
 
 }));
 app.use(passport.initialize());
+app.use(passport.session());
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { failureRedirect: '/' }),
+  passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/' }),
   (req, res) => {
     res.redirect('/');
   }
@@ -38,6 +44,9 @@ app.get('/auth/twitter/callback',
 
 require('./routes/poll.server.routes.js')(app);
 require('./routes/index.server.routes.js')(app);
+require('./routes/auth.server.routes.js')(app);
+
+app.use(express.static('public'));
 
 server = app.listen(config.port);
 console.log('Server is listening on', config.port);
