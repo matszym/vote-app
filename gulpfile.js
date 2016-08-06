ff62df6ce6e3abbcf74e081e11e410d13390c18c
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-07 00:00:38 +0200

    Configure gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp'),
 sourcemaps = require('gulp-sourcemaps'),
 concat = require('gulp-concat'),
 babel = require('gulp-babel'),
+browserSync = require('browser-sync');
 
 paths = {
   jsFiles: [
+    'client/**/*module.js',
     'client/*.js',
     'client/**/*.js']
 };
@@ -19,3 +21,18 @@ gulp.task('javascript', () => {
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('public'));
 });
+
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    open: 'external',
+    host: '127.0.0.1',
+    port: '9000',
+    proxy: 'http://127.0.0.1:9000'
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.jsFiles, ['javascript', 'browser-sync']);
+});
+
+gulp.task('default', ['javascript', 'watch', 'browser-sync']);
