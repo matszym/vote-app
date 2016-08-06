37ca67102b6890785cc6ae958ce0b742f72b2d35
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-07 00:05:22 +0200

    change callback url to comply with browser sync

--- config/development/config.js
+++ config/development/config.js
@@ -4,7 +4,7 @@ module.exports = {
   twitter: {
     consumerKey: process.env.VOTE_APP_TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.VOTE_APP_TWITTER_SECRET_KEY,
-    callbackURL: 'http://127.0.0.1:9000/auth/twitter/callback'
+    callbackURL: 'http://127.0.0.1:9001/auth/twitter/callback'
   },
   sessionSecret: 'Unsecure localhost secret'
 }
