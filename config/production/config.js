037559a04c5ff8be8cd3a3e6236bd3d73610e359
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-08-19 15:22:14 +0200

    Production config

---
+++ config/production/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  port: process.env.PORT,
+  domain: 'voting-app-matszym.herokuapp.com',
+  mongo: process.env.MONGODB_URI,
+  twitter: {
+    consumerKey: process.env.VOTE_APP_TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.VOTE_APP_TWITTER_SECRET_KEY,
+    callbackURL: 'https://' + 'voting-app-matszym.herokuapp.com:' + process.env.PORT + '/auth/twitter/callback'
+  },
+  sessionSecret: process.env.SESSION_SECRET
+}
